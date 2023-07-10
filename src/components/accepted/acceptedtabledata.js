e0d8f5dfd3f46ff8060a130dca9c9778ee59bcfd
prathmesh-66 <109199070+prathmesh-66@users.noreply.github.com>
2023-07-10 10:57:18 +0530

    Add files via upload

---
+++ src/components/accepted/acceptedtabledata.js
@@ -0,0 +1,25 @@
+
+
+const acceptedtabledata = [
+    {
+        name:"Candidate 1",
+        time:"10 minutes ago",
+        HSR:"HSR Layout",
+        gender:"Male"
+    },
+    {
+        name:"Candidate 1",
+        time:"10 minutes ago",
+        HSR:"HSR Layout",
+        gender:"Male"
+    },
+    {
+        name:"Candidate 1",
+        time:"10 minutes ago",
+        HSR:"HSR Layout",
+        gender:"Male"
+    },
+    
+];
+
+export default acceptedtabledata
